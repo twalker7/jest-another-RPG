0a6d9143ad2d06286246f9e6d71a660240d839e9
Thomas Walker <thomasnwalker@yahoo.com>
2021-06-25 23:35:53 -0700

    Potion.test.js test made to check potion.name & potion.value

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,4 +1,6 @@
 test('creates  health potion object', ()=>{
+    const potion = new Potion('health');
+
     expect(potion.name).toBe('health');
     expect(potion.value).toEqual(expect.any(Number))
 });
